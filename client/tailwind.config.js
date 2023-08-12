5eb91db0aa94f2768525f79a4c30f3918a996e5a
Christopher Jin <chrisjin1@gmail.com>
2023-08-12 13:26:43 -0400

    added themes as test

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            gridTemplateColumns: {
-                fluid: 'repeat(auto-fit, minmax(320px, 1fr))',
-            },
-        },
-    },
-    plugins: [require('daisyui'), require('@tailwindcss/forms')], // eslint-disable-line global-require
-    daisyui: {
-      themes:["light","dark"]
-    }
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+      extend: {
+          gridTemplateColumns: {
+              fluid: 'repeat(auto-fit, minmax(320px, 1fr))',
+          },
+      },
+      colors: {
+        'primaryLight': '#e9e9e5',
+        'secondaryLight': '#051e36',
+        'tertiaryLight': '#61A0AF',
+        'dangerLight': '#e63946',
+        'warningLight': '#f89e54',
+        'successLight': '#aafb9d',
+        'infoLight': '#96C9DC',
+      }
+  },
+  plugins: [require('daisyui'), require('@tailwindcss/forms')], // eslint-disable-line global-require
+  daisyui: {
+    
+  }
 };
